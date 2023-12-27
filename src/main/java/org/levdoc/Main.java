6751820d42508d1d4420fa8687dab37a161b4e81
Levchenko <levdoc@live.ru>
2023-12-27 09:09:02 +0300

    Initial Commit - 2

--- src/main/java/org/levdoc/Main.java
+++ src/main/java/org/levdoc/Main.java
@@ -1,7 +1,7 @@
 package org.levdoc;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void calc(String[] args) {
         System.out.println("Hello world!");
     }
 }
