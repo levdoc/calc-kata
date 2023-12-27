30fa1d2d5c746f6382e30d0dd845942353958a7c
levdoc <levdoc@live.ru>
2023-12-27 22:53:22 +0300

    Create RomanNumber Enum

---
+++ src/main/java/org/levdoc/RomanNumber.java
@@ -0,0 +1,27 @@
+package org.levdoc;
+
+enum RomanNumber {
+    I(1),
+    IV(4),
+    V(5),
+    IX(9),
+    X(10),
+    XL(40),
+    L(50),
+    XC(90),
+    C(100),
+    CD(400),
+    D(500),
+    CM(900),
+    M(1000);
+
+    private final int value;
+
+    RomanNumber(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
