8f1785b1421975c8d4f0283110282645af43a472
levdoc <levdoc@live.ru>
2023-12-27 22:54:37 +0300

    Create arabicToRoman method

--- src/main/java/org/levdoc/Main.java
+++ src/main/java/org/levdoc/Main.java
@@ -1,12 +1,39 @@
 package org.levdoc;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class Main {
+    static List<RomanNumber> romanNumberList = Arrays.asList(RomanNumber.values());
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(arabicToRoman(93));
     }
 
-    public static String calc (String input) {
+    public static String calc(String input) {
         return null;
+    }
+
+    /**
+     * Метод конвертирует число в римской записи в арабскую
+     *
+     * @param value римское число
+     * @return арабское число
+     */
+    private static String arabicToRoman(int value) {
+        Collections.reverse(romanNumberList);
+
+        StringBuilder result = new StringBuilder();
+        for (RomanNumber romanNumber : romanNumberList) {
+            while (value >= romanNumber.getValue()) {
+                result.append(romanNumber);
+                value -= romanNumber.getValue();
+            }
+        }
+
+        return result.toString();
     }
 
 }
